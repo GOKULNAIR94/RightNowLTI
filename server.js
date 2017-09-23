37d91f96b393fdfea7d9cee2a062112676bd1c51
Gokul Nair <gokulgnair94@gmail.com>
2017-09-24 01:41:34 +0530

    RN

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+var http = require('https'),
+    fs = require('fs');
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+
+var serialNum = "";
+var assetId = "";
+
+restService.post('/', function(req, res) {
+    serialNum = request.query.serialnum;
+    assetId = request.query.assetid;
+
+    console.log( "serialNum : " + serialNum );
+    console.log( "assetId : " + assetId );
+});
+
+restService.listen((process.env.PORT || 9000), function() {
+    console.log("Server up and listening");
+});
