9add2260da6b493888dab9e22479a34908e5f138
Gokul Nair <gokulgnair94@gmail.com>
2017-09-24 12:58:56 +0530

    Get Done

--- query.js
+++ query.js
@@ -25,7 +25,7 @@ module.exports = function ( qString, req, res, callback){
           //console.log("Body : " + responseString);
           resObj = JSON.parse(responseString);
           if( resObj.items != null ){
-              var rowCount = resObj.count;
+              var rowCount = resObj.items.count;
               console.log( "rowCount : " + rowCount);
           }
           //console.log( "resObj : " + JSON.stringify(resObj));
