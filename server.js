4aa433014439641a7443b1c1b33e39667421161e
Gokul Nair <gokulgnair94@gmail.com>
2017-09-24 09:32:17 +0530

    RN

--- server.js
+++ server.js
@@ -13,17 +13,30 @@ restService.use(bodyParser.json());
 
 var serialNum = "";
 var assetId = "";
+var queryField;
 
 restService.post('/', function(req, res) {
-    serialNum = req.query.serialnum;
-    assetId = req.query.assetid;
+    
+    if( (Serial_Number == null || Serial_Number == "") && (PumpAssetId == null || PumpAssetId == "") ) {
+        //Error
+    }
+    else{
+        if( (Serial_Number == null && Serial_Number == "")  ) {
+            queryField = "PumpAssetId";
+            queryValue = req.query.assetid;
+            console.log( "PumpAssetId : " + PumpAssetId );
+        }
+        else{
+            queryField = "Serial_Number";
+            queryValue = req.query.serialnum;
+            console.log( "Serial_Number : " + Serial_Number );
+        }
 
-    console.log( "serialNum : " + serialNum );
-    console.log( "assetId : " + assetId );
-    res.json({
-        "serialNum" : serialNum,
-        "assetId" : assetId
-    });
+        res.json({
+            queryField : queryValue
+        });
+    }
+    
 });
 
 restService.listen((process.env.PORT || 9000), function() {
