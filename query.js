1752a5a884c0bd55b62f3a2bd10043f811a65e96
Gokul Nair <gokulgnair94@gmail.com>
2017-09-24 12:20:44 +0530

    RN

--- query.js
+++ query.js
@@ -22,11 +22,11 @@ module.exports = function ( qString, req, res, callback){
       });
 
       res.on("end", function () {
-          console.log("Body : " + responseString);
+          //console.log("Body : " + responseString);
           resObj = JSON.parse(responseString);
           var rowCount = resObj.count;
           console.log( "rowCount : " + rowCount);
-          console.log( "resObj : " + JSON.stringify(resObj));
+          //console.log( "resObj : " + JSON.stringify(resObj));
           callback( resObj );
       });
     });
